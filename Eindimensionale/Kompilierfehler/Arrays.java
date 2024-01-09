46673be34a99cb100721439a59dc33d5aec08d12
Pejwak <108393406+Pejwak@users.noreply.github.com>
2024-01-09 21:04:58 +0100

    Delete Eindimensionale directory

--- Eindimensionale/Kompilierfehler/Arrays.java
+++
@@ -1,9 +0,0 @@
-package Arrays.Eindimensionale.Kompilierfehler;
-
-public class Arrays {
-
-	public static void main(String[] args) {
-		int b[] = new int[];		
-	}
-}
-
--- Eindimensionale/Kompilierfehler/Arrays2.java
+++
@@ -1,9 +0,0 @@
-package Arrays.Eindimensionale.Kompilierfehler;
-
-public class Arrays2 {
-	
-	public static void main(String[] args) {
-		int b[5] = new int[5]; 
-	}
-}
-
